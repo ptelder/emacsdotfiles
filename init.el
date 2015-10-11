fbf43d31dbd13d8d3724a492c644da10aa25fed9
Kenneth W Brown III <pliny@ptelder.net>
2015-10-10 17:50:09 -0700

    Forcing early-in-startup disable of GUI elements,default buffers, and package init

--- init.el
+++ init.el
@@ -1,4 +1,22 @@
+;; Org-Mode .emacs setup
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+;; Adjust default buffers
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "Always mount a scratch buffer?")
+
+;; Get package up and ready to go
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
+
 (require 'smex) ; Not needed if you use package.el
 (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
                   ; when Smex is auto-initialized on its first run.
