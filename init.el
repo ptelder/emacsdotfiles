49e731c2622938c66d369adcd9f34cd24f012b84
Kenneth W Brown III <pliny@ptelder.net>
2015-10-10 19:35:15 -0700

    Initial setup of org-mode config file and transfer of non-essential lines out of init.el

--- init.el
+++ init.el
@@ -26,15 +26,5 @@
 (eval-when-compile
   (require 'use-package))
 
-(require 'smex) ; Not needed if you use package.el
-(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
-                  ; when Smex is auto-initialized on its first run.
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-(setq custom-file (expand-file-name "customize-persistence.el" user-emacs-directory))
-(load custom-file)
-
+;;; Untangle and load org-mode config
+(org-babel-load-file (concat user-emacs-directory "emacs_config.org"))
