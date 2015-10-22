f55e131ffbb118ee214699128a08e8eb474e4372
Kenneth W Brown III <pliny@ptelder.net>
2015-10-22 13:31:15 -0700

    Fixed settings to make customize happy for now.

--- customize-persistence.el
+++ customize-persistence.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (birds-of-paradise-plus)))
+ '(custom-enabled-themes (quote (birds-of-paradise-plus smart-mode-line-dark)))
  '(custom-safe-themes
    (quote
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "44eec3c3e6e673c0d41b523a67b64c43b6e38f8879a7969f306604dcf908832c" default)))
