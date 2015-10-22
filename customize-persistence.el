7ce06d25cca9cce52ce7f53aae39fc18db44fbd2
Kenneth W Brown III <pliny@ptelder.net>
2015-10-22 13:17:56 -0700

    Okayed a theme

--- customize-persistence.el
+++ customize-persistence.el
@@ -6,7 +6,7 @@
  '(custom-enabled-themes (quote (birds-of-paradise-plus)))
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "44eec3c3e6e673c0d41b523a67b64c43b6e38f8879a7969f306604dcf908832c" default)))
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "44eec3c3e6e673c0d41b523a67b64c43b6e38f8879a7969f306604dcf908832c" default)))
  '(ido-mode (quote both) nil (ido))
  '(menu-bar-mode nil)
  '(nyan-animate-nyancat t)
